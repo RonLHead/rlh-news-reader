3f8d00b18bea29d55d0b59f9421eed2acd228a90
Ronald Head <ronaldlhead@gmail.com>
2022-06-07 17:03:34 -0600

    Set up Nav directory in components

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import './Nav.css';
+
+const Nav = () => {
+  return (
+    <nav className='Nav-bar'>
+      <div className='Nav-container'>
+        <h2 className='Nav-text'>Home</h2>
+        <h2>Saved Stories</h2>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav;
