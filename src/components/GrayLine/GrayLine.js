f14d109280e7951103cea92c8d102fcc4d217d16
Ronald Head <ronaldlhead@gmail.com>
2022-06-08 12:32:01 -0600

    Add GrayLine directory

---
+++ src/components/GrayLine/GrayLine.js
@@ -0,0 +1,15 @@
+const GrayLine = () => {
+  return (
+    <hr 
+      style={{
+        backgroundColor: '#F1F2F2',
+        border: 'none',
+        borderRadius: '5px',
+        height: 5,
+        width: '98vw',
+      }}
+    />
+  )
+}
+
+export default GrayLine;
