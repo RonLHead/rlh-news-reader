d55a3c5ac04429e9f43032b537f8422cf9180f01
Ronald Head <ronaldlhead@gmail.com>
2022-06-08 14:32:55 -0600

    Add NavLink to home on Home text

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,16 @@
+import { NavLink } from 'react-router-dom';
 import './Nav.css';
 
 const Nav = () => {
   return (
     <nav className='Nav-bar'>
-      <div className='Nav-container'>
-        <h2 className='Nav-text'>Home</h2>
-        <h2>Saved Stories</h2>
-      </div>
-    </nav>
+        <div className='Nav-container'>
+          <NavLink to='/' style={{ textDecoration: 'none', color: 'white' }}>
+            <h2 className='home-text'>Home</h2>
+          </NavLink>
+          <h2 className='saved-stories-text'>Saved Stories</h2>
+        </div>
+      </nav>
   )
 }
 
