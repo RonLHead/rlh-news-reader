89f6e4b46447565844df7d2d13c7d9d56f5e74be
Ronald Head <ronaldlhead@gmail.com>
2022-06-08 14:33:36 -0600

    Add NavLink to home on title

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,15 @@
 import Nav from '../Nav/Nav';
 import Stories from '../Stories/Stories';
+import { NavLink } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
   return (
     <main>
       <header className='App-header'>
-        <h1 className='header-title'>My News Reader</h1>
+        <NavLink to='/' style={{ textDecoration: 'none', color: 'white' }}>
+          <h1 className='header-title'>My News Reader</h1>
+        </NavLink>
       </header>
       <Nav />
       <Stories />
