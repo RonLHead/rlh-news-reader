3f164505adeb39fb0988e15563ed3a7ea88e62ca
Ronald Head <ronaldlhead@gmail.com>
2022-06-08 18:06:16 -0600

    Move state up from Stories to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { fetchStories } from '../../apiCalls';
+
 import Nav from '../Nav/Nav';
 import Stories from '../Stories/Stories';
 import { NavLink } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import SingleStory from '../SingleStory/SingleStory';
 import './App.css';
 
 const App = () => {
+  const [stories, setStories] = useState([]);
+  const [error, setError] = useState('');
+  const [topStory, setTopStory] = useState({});
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getStories = () => {
+    fetchStories()
+      .then(data => {
+        return (
+          setTopStory(data.results.shift()),
+          setStories(data.results),
+
+          setIsLoading(false)
+        )
+      })
+      .catch(err => 
+        setError('Oops, something went wrong. Please try again later.')
+      )
+  }
+
+  useEffect(() => {
+    getStories()
+  }, [])
+
   return (
     <main>
       <header className='App-header'>
@@ -12,7 +41,11 @@ const App = () => {
         </NavLink>
       </header>
       <Nav />
-      <Stories />
+      <Routes>
+        <Route index element={<Stories stories={stories} error={error} topStory={topStory} isLoading={isLoading}/>} />
+        <Route path=':id' element={<SingleStory story={stories}/>} />
+      
+      </Routes>
     </main>
   );
 }
