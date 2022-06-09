ab1cf8ac1557ebcd8dc358b6f597fb69d87fbe20
Ronald Head <ronaldlhead@gmail.com>
2022-06-09 00:08:51 -0600

    Refactor TopStory component

--- src/components/TopStory/TopStory.js
+++ src/components/TopStory/TopStory.js
@@ -1,15 +1,25 @@
+import React from 'react';
+import Error from '../Error/Error';
 import './TopStory.css';
 
 const TopStory = (props) => {
+  let topStorySection;
+  if(!props.topStory.multimedia) {
+    topStorySection = <Error />
+  } else {
+    topStorySection = (
+      <section className='TopStory-container'>
+        <img className='TopStory-img' alt={props.topStory.multimedia[1].caption} src={props.topStory.multimedia[1].url} />
+        <div className='TopStory-info-contianer'>
+          <h2 className='TopStory-title'>{props.topStory.title}</h2>
+          <p className='TopStory-abstract'>{props.topStory.abstract}</p>
+          <p>{props.topStory.byline}</p>
+        </div>
+      </section>
+    )
+  }
   return (
-    <section className='TopStory-container'>
-      <img className='TopStory-img' alt={props.topStory.multimedia[1].caption} src={props.topStory.multimedia[1].url}/>
-      <div className='TopStory-info-contianer'>
-        <h2 className='TopStory-title'>{props.topStory.title}</h2>
-        <p className='TopStory-abstract'>{props.topStory.abstract}</p>
-        <p>{props.topStory.byline}</p>
-      </div>
-    </section>
+    <div>{topStorySection}</div>
   )
 }
 
