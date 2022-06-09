98def24178c5380396ac6061943bfd4c7567ff1b
Ronald Head <ronaldlhead@gmail.com>
2022-06-08 23:00:18 -0600

    Refactor fetch call to interpolate section

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,6 @@
 const key = process.env.REACT_APP_API_KEY;
 
-export const fetchStories = () => {
-  return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${key}`)
+export const fetchStories = (section) => {
+  return fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${key}`)
     .then(response => response.json())
 }
